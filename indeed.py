78b56c947ec8a8eead626a5a1c29b9a70ad91256
ningpop <craft1933@naver.com>
2020-09-22 01:30:28 +0900

    #2.9 StackOverflow Pages

--- indeed.py
+++ indeed.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 LIMIT = 50
 URL = f'https://www.indeed.com/jobs?q=python&limit={LIMIT}'
 
-def extract_indeed_pages():
+def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, 'html.parser')
     pagination = soup.find("div", {"class":"pagination"})
@@ -14,25 +14,26 @@ def extract_indeed_pages():
 
     for link in links[:-1]:
         pages.append(int(link.string))
-
     max_page = pages[-1]
-
     return max_page
 
 def extract_job(html):
     title = html.find("h2", {"class":"title"}).find("a")["title"]
     company = html.find("span", {"class":"company"})
-    company_anchor = company.find("a")
-    if company.find("a") is not None:
-        company = str(company_anchor.string)
+    if company:
+        company_anchor = company.find("a")
+        if company.find("a") is not None:
+            company = str(company_anchor.string)
+        else:
+            company = str(company.string)
+        company = company.strip()
     else:
-        company = str(company.string)
-    company = company.strip()
+        company = None
     location = html.find("div", {"class":"recJobLoc"})["data-rc-loc"]
     job_id = html["data-jk"]
     return {"title":title, "company":company, "location":location, "link":f"https://www.indeed.com/viewjob?jk={job_id}"}
 
-def extract_indeed_jobs(last_page):
+def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
         print(f"Scrapping page {page}")
@@ -42,4 +43,9 @@ def extract_indeed_jobs(last_page):
         for result in results:
             job = extract_job(result)
             jobs.append(job)
+    return jobs
+
+def get_jobs():
+    last_page = get_last_page()
+    jobs = extract_jobs(last_page)
     return jobs
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from indeed import extract_indeed_pages, extract_indeed_jobs
+''' indeed scrapping '''
+# from indeed import extract_indeed_pages, extract_indeed_jobs
 
+# last_indeed_page = extract_indeed_pages()
+# indeed_jobs = extract_indeed_jobs(last_indeed_page)
+# print(indeed_jobs)
 
-last_indeed_page = extract_indeed_pages()
+''' stackoverflow scrapping '''
+from indeed import get_jobs as get_indeed_jobs
+from so import get_jobs as get_so_jobs
 
-indeed_jobs = extract_indeed_jobs(last_indeed_page)
-
-print(indeed_jobs)
+# indeed_jobs = get_indeed_jobs()
+so_jobs = get_so_jobs()
