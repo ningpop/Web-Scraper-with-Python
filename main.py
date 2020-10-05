a9077f103df95f7f87d48f07eccb9992b856da0b
ningpop <craft1933@naver.com>
2020-10-05 18:05:50 +0900

    #4.1 Introduction to Flask

--- main.py
+++ main.py
@@ -6,11 +6,29 @@
 # print(indeed_jobs)
 
 """ stackoverflow scrapping """
-from indeed import get_jobs as get_indeed_jobs
-from so import get_jobs as get_so_jobs
-from save import save_to_file
+# from indeed import get_jobs as get_indeed_jobs
+# from so import get_jobs as get_so_jobs
+# from save import save_to_file
 
-so_jobs = get_so_jobs()
-indeed_jobs = get_indeed_jobs()
-jobs = so_jobs + indeed_jobs
-save_to_file(jobs)
+# so_jobs = get_so_jobs()
+# indeed_jobs = get_indeed_jobs()
+# jobs = so_jobs + indeed_jobs
+# save_to_file(jobs)
+
+""" Web Scrapper with Flask """
+from flask import Flask
+
+app = Flask("SuperScrapper")
+
+# @: decorator(데코레이터), 함수를 꾸며주는 역할
+@app.route("/")
+def home():
+    return "Hello! Welcome to mi casa!"
+
+
+@app.route("/contact")
+def potato():
+    return "Contact me!"
+
+
+app.run(host="0.0.0.0")
